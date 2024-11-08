eb87a0a3067ecae6769bdd43a51c41c6fc793540
Stark16 <pradyumnp508@gmail.com>
2024-11-07 18:23:25 -0600

    added pipeline testing

--- MatSciBERT/test_mlm.py
+++ MatSciBERT/test_mlm.py
@@ -3,7 +3,7 @@ import torch
 import matplotlib.pyplot as plt
 import argparse
 import os
-from transformers import BertForMaskedLM, BertTokenizer, AutoConfig, AutoTokenizer
+from transformers import BertForMaskedLM, BertTokenizer, AutoConfig, AutoTokenizer, pipeline
 from colorama import Fore, Back, Style
 from tqdm import tqdm
 from ner.NER_inference import NER_INF
@@ -15,36 +15,13 @@ class TestingMLM:
     def __init__(self) -> None:
         self.PATH_self_dir = os.path.dirname(os.path.realpath(__file__))
         self.PATH_bert_cache = os.path.join(self.PATH_self_dir, 'bert_temp_cache')
-        self.PATH_results_dir = os.path.join(self.PATH_self_dir, 'mlm_tests/2005_model')
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         self.OBJ_ner_inf = NER_INF()
         self.MODEL_ner = self.OBJ_ner_inf.initialize_infer()
         self.ENTITYCLS = self.OBJ_ner_inf.label_list
-        self.NUM_NONTECHPERSENT = 4
-
-    # def load_bert(self, model_name):
-    #     model_revision = 'main'
-
-    #     tokenizer_kwargs = {
-    #         'cache_dir': self.PATH_bert_cache,
-    #         'use_fast': True,
-    #         'revision': model_revision,
-    #         'use_auth_token': None,
-    #     }
-    #     bert_tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_kwargs)
-
-    #     bert_config_kwargs = {
-    #         'cache_dir': self.PATH_bert_cache,
-    #         'revision': model_revision,
-    #         'use_auth_token': None,
-    #     }
-    #     bert_config = AutoConfig.from_pretrained(model_name, **bert_config_kwargs)
-    #     model = BertForMaskedLM.from_pretrained(model_name, from_tf=False, config=bert_config, cache_dir=self.PATH_bert_cache, 
-    #                                             revision=model_revision, use_auth_token=None,)
-    #     model.resize_token_embeddings(len(bert_tokenizer))
-    #     model.to(torch.device(self.device))  
+        self.NUM_NONTECHPERSENT = 4 
 
     def load_bert(self, m_dir):
         tokenizer = BertTokenizer.from_pretrained(m_dir)
@@ -60,7 +37,6 @@ class TestingMLM:
         masked_sentences = {}
         lower_sentence = sentence.lower()
 
-        # Gather non-technical tokens to limit them later if needed
         non_technical_tokens = []
 
         for token, label in entity_labels.items():
@@ -74,11 +50,9 @@ class TestingMLM:
                     masked_sentence = lower_sentence.replace(lower_token, '[MASK]', 1)
                     masked_sentences[masked_sentence] = token
                 elif label == 'O' and mask_non_technical and lower_token in lower_sentence:
-                    # Only add non-technical tokens that are words (remove punctuation and special characters)
                     if re.match(r"^[a-zA-Z0-9]+$", lower_token):  
                         non_technical_tokens.append(lower_token)
 
-        # Limit non-technical tokens to a reasonable subset, e.g., 3 random selections
         if mask_non_technical and non_technical_tokens:
             sample_tokens = random.sample(non_technical_tokens, min(len(non_technical_tokens), 3))
             for token in sample_tokens:
@@ -86,20 +60,38 @@ class TestingMLM:
                 masked_sentences[masked_sentence] = token
 
         return masked_sentences
+    
+    def predict_masked_token(self, masked_sentence, model, tokenizer, use_pipeline=False):
+        if use_pipeline:
+            classifier = pipeline("fill-mask", model=model, tokenizer=tokenizer, device=self.device)
+            results = classifier(masked_sentence)
+            predicted_token = results[0]["token_str"]
+        else:
+            inputs = tokenizer(masked_sentence, return_tensors="pt").to(self.device)
+            with torch.no_grad():
+                outputs = model(**inputs)
+                logits = outputs.logits
 
-    def predict_masked_token(self, masked_sentence, model, tokenizer):
-        inputs = tokenizer(masked_sentence, return_tensors="pt").to(self.device)
-        with torch.no_grad():
-            outputs = model(**inputs)
-            logits = outputs.logits
-
-        mask_token_index = torch.where(inputs.input_ids == tokenizer.mask_token_id)[1]
-        mask_logits = logits[0, mask_token_index, :]
-        predicted_token_id = torch.argmax(mask_logits, dim=-1)
-        predicted_token = tokenizer.decode(predicted_token_id)
+            mask_token_index = torch.where(inputs.input_ids == tokenizer.mask_token_id)[1]
+            mask_logits = logits[0, mask_token_index, :]
+            predicted_token_id = torch.argmax(mask_logits, dim=-1)
+            predicted_token = tokenizer.decode(predicted_token_id)
+            
         return predicted_token
+    
+    # def predict_masked_token(self, masked_sentence, model, tokenizer):
+    #     inputs = tokenizer(masked_sentence, return_tensors="pt").to(self.device)
+    #     with torch.no_grad():
+    #         outputs = model(**inputs)
+    #         logits = outputs.logits
 
-    def run_inference_on_sentences(self, sentences, model, tokenizer, mask_non_technical=False):
+    #     mask_token_index = torch.where(inputs.input_ids == tokenizer.mask_token_id)[1]
+    #     mask_logits = logits[0, mask_token_index, :]
+    #     predicted_token_id = torch.argmax(mask_logits, dim=-1)
+    #     predicted_token = tokenizer.decode(predicted_token_id)
+    #     return predicted_token
+
+    def run_inference_on_sentences(self, sentences, model, tokenizer, mask_non_technical=False, use_pipeline=False):
         correct_predictions = 0
         total_predictions = 0
         results = []
@@ -110,7 +102,7 @@ class TestingMLM:
             
             masked_sentences = self.mask_entities(sentence, entity_labels, mask_non_technical=mask_non_technical)
             for masked_sentence, original_word in masked_sentences.items():
-                predicted_token = self.predict_masked_token(masked_sentence, model, tokenizer)
+                predicted_token = self.predict_masked_token(masked_sentence, model, tokenizer, use_pipeline=True)
 
                 original_word_colored = Fore.RED + original_word + Fore.WHITE
                 masked_sentence_colored = masked_sentence.replace('[MASK]', original_word_colored)
@@ -122,8 +114,8 @@ class TestingMLM:
                     predicted_token_colored = Fore.MAGENTA + predicted_token + Fore.WHITE
                 total_predictions += 1
 
-                print(Back.BLACK + "\tOriginal Sentence with MASK: ", Back.WHITE + masked_sentence_colored, Back.BLACK)
-                print("\tPredicted Sentence: ", masked_sentence.replace('[MASK]', predicted_token_colored), Back.BLACK, '\n\n')
+                # print(Back.BLACK + "\tOriginal Sentence with MASK: ", Back.WHITE + masked_sentence_colored, Back.BLACK)
+                # print("\tPredicted Sentence: ", masked_sentence.replace('[MASK]', predicted_token_colored), Back.BLACK, '\n\n')
                 
                 results.append([sentence, masked_sentence.replace('[MASK]', predicted_token), original_word, predicted_token, 
                                 predicted_token.strip() == original_word.strip(), not mask_non_technical, entity_labels[original_word]])
@@ -132,7 +124,7 @@ class TestingMLM:
         print(f"Accuracy: {accuracy:.2f}% ({correct_predictions}/{total_predictions} correctly predicted)")
         return results
     
-    def plot_results(self, DF_results, test_configs):
+    def plot_results(self, DF_results, test_configs, filename):
         plt.figure(figsize=(12, 8))
 
         correct_counts = []
@@ -167,12 +159,14 @@ class TestingMLM:
         plt.title('Correct vs Incorrect Predictions by Model and Masking Mode')
         plt.legend()
 
-        plot_path = os.path.join(self.PATH_results_dir, '500ex_2005_tr_2005_80_32ge_prediction_accuracy_comparison.png')
+        plot_path = os.path.join(self.PATH_results_dir, f'{filename}_prediction_accuracy_comparison.png')
         plt.savefig(plot_path)
         plt.close()
 
 
-    def test(self, trained_bert_mlm, pth_txt, t_size):
+    def test(self, PATH_trained_bert_mlm, pth_txt, PATH_output, t_size, use_pipeline=False):
+
+        self.PATH_results_dir = os.path.join(self.PATH_self_dir, PATH_output)
         if not os.path.exists(self.PATH_results_dir):
             os.makedirs(self.PATH_results_dir)
         df_columns=['UnMaksed', 'Predicted', 'GT_Word', 'P_Word', 'IsCorrect', 'IsTechEntity', 'EntityCls', 'ModelName']
@@ -180,34 +174,41 @@ class TestingMLM:
 
         random_sentences = self.get_random_sentences(pth_txt, t_size)
         MODEL_bert, TOK_bert = self.load_bert('bert-base-uncased')
-        MODEL_mlm_bert, TOK_mlm_bert = self.load_bert(trained_bert_mlm)
+        MODEL_mlm_bert, TOK_mlm_bert = self.load_bert(PATH_trained_bert_mlm)
 
         test_configs = [(MODEL_bert, TOK_bert, False, 'base_bert_tech'), (MODEL_bert, TOK_bert, True, 'base_bert_non_tech'),
                         (MODEL_mlm_bert, TOK_mlm_bert, False, 'mlm_bert_tech'), (MODEL_mlm_bert, TOK_mlm_bert, True, 'mlm_bert_non_tech')]
 
         for model, tokenizer, mask_non_technical, model_name in tqdm(test_configs):
-            results = self.run_inference_on_sentences(random_sentences, model, tokenizer, mask_non_technical)
+            results = self.run_inference_on_sentences(random_sentences, model, tokenizer, mask_non_technical, use_pipeline)
             for result in results:
                 result.append(model_name) 
                 DF_results.loc[len(DF_results)] = result
-        csv_path = os.path.join(self.PATH_results_dir, '500ex_2005_tr_2005_80_32ge_test_results.csv')
+        csv_path = os.path.join(self.PATH_results_dir, f'{t_size}ex_{os.path.basename(os.path.dirname(pth_txt))}_{os.path.basename(os.path.dirname(PATH_trained_bert_mlm))}_test_results.csv')
         DF_results.to_csv(csv_path, index=False, sep='\t')
 
-        self.plot_results(DF_results, test_configs)
+        self.plot_results(DF_results, test_configs, filename=f'{t_size}ex_{os.path.basename(os.path.dirname(pth_txt))}_{os.path.basename(os.path.dirname(PATH_trained_bert_mlm))}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run inference on masked tokens in sentences.")
-    parser.add_argument('--m_dir', type=str, default="/home/ppathak/Hypothesis_Generation_Active_Learning/MatSciBERT/trained_model/tr_2005_80_32ge/checkpoint-1699", 
+    parser.add_argument('--m_dir', type=str, default="/home/ppathak/Hypothesis_Generation_Active_Learning/MatSciBERT/trained_model/tr_2005_80_32ge/checkpoint-1920", 
                         help="Directory of the trained model.")
-    parser.add_argument('--pth_txt', type=str, default="/home/ppathak/Hypothesis_Generation_Active_Learning/datasets/semantic_kg/json_dataset/2005/val_norm.txt", 
+    parser.add_argument('--pth_txt', type=str, default="/home/ppathak/Hypothesis_Generation_Active_Learning/datasets/semantic_kg/json_dataset/2015/normalized_val.txt", 
                         help="Path to the text file containing sentences.")
     parser.add_argument('--t_size', type=int, default=500, 
                         help="Number of sentences to sample from the text file.")
     parser.add_argument('--mask_non_technical', action='store_true', #default=True,
                         help="If set, masks non-technical words instead of technical entities.")
     
-    args = parser.parse_args()
-    PATH_model, PATH_val_txt, test_size, _ = args.m_dir, args.pth_txt, args.t_size, args.mask_non_technical
+    # args = parser.parse_args()
+    # PATH_model, PATH_val_txt, test_size, _ = args.m_dir, args.pth_txt, args.t_size, args.mask_non_technical
+
+    PATH_model = "/home/ppathak/Hypothesis_Generation_Active_Learning/MatSciBERT/trained_model/tr_2005_80_32ge/checkpoint-1920"
+    PATH_val_txt = "/home/ppathak/Hypothesis_Generation_Active_Learning/datasets/semantic_kg/json_dataset/2005/val_norm.txt"
+    PATH_output = 'mlm_tests/pipeline/2005_model'
+    test_size = 500
+    use_pipeline = True
+
     OBJ_MlmTest = TestingMLM()
-    OBJ_MlmTest.test(PATH_model, PATH_val_txt, test_size)
+    OBJ_MlmTest.test(PATH_model, PATH_val_txt, PATH_output, test_size, use_pipeline)
